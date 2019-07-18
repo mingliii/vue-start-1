942d9232b1b732d47a0bee166af16f5923a4cfbf
Ming Li <li.ming116@gmail.com>
2019-07-18 22:56:56 +0100

    first commit

---
+++ src/components/NewQuote.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="row">
+        <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3">
+            <lable>Quote</lable>
+            <textarea class="form-control" rows="3" v-model="quote"></textarea>
+        </div>
+        <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3">
+            <button class="btn btn-primary" @click.prevent="createNew">Add Quote</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "NewQuote",
+        data: function () {
+            return {
+                quote: ''
+            }
+        },
+        methods: {
+            createNew() {
+                if(!this.quote) {
+                    return alert('Please enter quote!');
+                }
+                this.$emit('quoteAdded', this.quote);
+                this.quote = '';
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
